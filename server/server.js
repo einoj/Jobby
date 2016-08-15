8261a02ae71a433aed7317a3a6a0dcf61d8b4153
einoj <einoju@gmail.com>
2016-08-15 22:35:50 +0200

    unident library lines

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
- const http = require('http');
- const net = require('net');
- const url = require('url');
+const http = require('http');
+const net = require('net');
+const url = require('url');
 
 var proxy  = http.createServer( (req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
