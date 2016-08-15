03be9ceaa57f4a40f711f4fb227fba3ae29bdf8b
Eino <einoj@users.noreply.github.com>
2016-08-15 22:26:47 +0200

    Added simple server

---
+++ server/server.js
@@ -0,0 +1,12 @@
+ const http = require('http');
+ const net = require('net');
+ const url = require('url');
+
+var proxy  = http.createServer( (req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+
+  res.end('Hello World\n');
+}).listen(8081);
+
+console.log('Server running at http://127.0.0.1:8081/');
+
